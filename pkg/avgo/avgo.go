d6b47270b1801a6f4937715650362bec0f323222
samiam376 <ssheridan211@gmail.com>
2022-04-22 15:59:26 -0700

    wrote basic tests

--- pkg/avgo/avgo.go
+++ pkg/avgo/avgo.go
@@ -1,24 +1,24 @@
 package avgo
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"sort"
 )
 
 //price struct defines price at point in time
 type Price struct {
-	Open   float64 `json:"1. open"`
-	High   float64 `json:"2. high"`
-	Low    float64 `json:"3. low"`
-	Close  float64 `json:"4. close"`
-	Volume float64 `json:"5. volume"`
+	Open   float64 `json:"1. open,string"`
+	High   float64 `json:"2. high,string"`
+	Low    float64 `json:"3. low,string"`
+	Close  float64 `json:"4. close,string"`
+	Volume float64 `json:"5. volume,string"`
 }
 
 //timeseries is a map of a time to a price struct
-type Timeseries struct {
-	Time map[string]Price `json:"Time Series (5min)"`
-}
+type Timeseries map[string]Price
 
 //response metadata
 type Metadata struct {
@@ -31,9 +31,35 @@ type Metadata struct {
 }
 
 //IntraDay Pricing Api Call response
+// Timeseries is not neccisarily sorted
 type IntraDayResponse struct {
 	MD Metadata   `json:"Meta Data"`
 	TS Timeseries `json:"Time Series (5min)"`
+}
+
+//Read only container that holds the sorted keys to the timeseries and the underlying map
+//these can be used to iterate over the container in order
+type SortedSeries struct {
+	unsortedContainer Timeseries
+	SortedKeys        []string
+}
+
+func NewSortedSeries(unsorted Timeseries) *SortedSeries {
+	//copy over unsorted map to prevent mutations on the underlying data
+	container := Timeseries{}
+	sortedKeys := make([]string, 0)
+	for key, val := range unsorted {
+		container[key] = val
+		sortedKeys = append(sortedKeys, key)
+	}
+	//sort the keys
+	sort.Strings(sortedKeys)
+	//return a new object
+	return &SortedSeries{unsortedContainer: container, SortedKeys: sortedKeys}
+}
+
+func (s *SortedSeries) Get(key string) Price {
+	return s.unsortedContainer[key]
 }
 
 //interface to call alpha vantage api
@@ -42,8 +68,14 @@ type AvGoClient interface {
 }
 
 //unmarshal api response into Timerseries
-func unmarshallIntraDayPricesToTimeseries(body []byte) (*Timeseries, error) {
-	return nil, nil
+func unmarshallIntraDayPricesToSortedSeries(body []byte) (*SortedSeries, error) {
+	resp := &IntraDayResponse{}
+	err := json.Unmarshal(body, resp)
+	if err != nil {
+		return nil, err
+	}
+	ss := NewSortedSeries(resp.TS)
+	return ss, nil
 }
 
 //formated query string to include stock symbol and api key
@@ -53,11 +85,15 @@ func formatIntraDayRequest(api_key string, symbol string) string {
 	return formatted
 }
 
-func RequestIntraDayPrices(api_key string, symbol string, client AvGoClient) (*Timeseries, error) {
+func RequestIntraDayPrices(api_key string, symbol string, client AvGoClient) (*SortedSeries, error) {
 	request := formatIntraDayRequest(api_key, symbol)
 	response, err := client.Get(request)
 	if err != nil {
 		return nil, err
+	}
+
+	if response.StatusCode != 200 {
+		return nil, fmt.Errorf("API Call failed with status code %d", response.StatusCode)
 	}
 
 	bodyString, err := io.ReadAll(response.Body)
@@ -65,12 +101,12 @@ func RequestIntraDayPrices(api_key string, symbol string, client AvGoClient) (*T
 		return nil, err
 	}
 
-	ts, err := unmarshallIntraDayPricesToTimeseries(bodyString)
+	ss, err := unmarshallIntraDayPricesToSortedSeries(bodyString)
 
 	if err != nil {
 		return nil, err
 	}
 
-	return ts, nil
+	return ss, nil
 
 }
--- pkg/avgo/avgo_test.go
+++ pkg/avgo/avgo_test.go
@@ -1,1 +1,45 @@
 package avgo
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"testing"
+)
+
+func TestFormatRequest(t *testing.T) {
+	api_key := "fakekey"
+	symbol := "IBM"
+	expected := "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=fakekey"
+	actual := formatIntraDayRequest(api_key, symbol)
+
+	if expected != actual {
+		t.Error("expected did not equal actual")
+	}
+
+}
+
+func TestUnmarshallIntraDayPricesToSortedSeries(t *testing.T) {
+	data, _ := ioutil.ReadFile("mock_response.json")
+	_, err := unmarshallIntraDayPricesToSortedSeries(data)
+	if err != nil {
+		t.Errorf("failed to unmarshal with error: %s", err)
+	}
+}
+
+type mockClient struct{}
+
+func (m mockClient) Get(input string) (*http.Response, error) {
+	data, _ := ioutil.ReadFile("mock_response.json")
+	r := ioutil.NopCloser(bytes.NewReader(data))
+	response := &http.Response{StatusCode: 200, Body: r}
+	return response, nil
+}
+
+func TestRequestIntraDayPrices(t *testing.T) {
+	client := &mockClient{}
+	_, err := RequestIntraDayPrices("mockkey", "IBM", client)
+	if err != nil {
+		t.Error("failed")
+	}
+}
